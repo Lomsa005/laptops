ee7ab6c675056d2db464617561fd23b0131b85e8
saba lomsadze <103242407+lawliet005@users.noreply.github.com>
2022-04-11 11:30:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -39,31 +39,3 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
-let countDate = new Date('june 1, 2021 00:00:00').getTime();
-
-function countDown(){
-
-    let now = new Date().getTime();
-
-    gap = countDate - now;
-
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
-
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
-
-}
-
-setInterval(function(){
-    countDown();
-},1000)
